df6ba8833cd89ec41240f1825fd31b09ccc62ed1
fscarapaica <fs.carapaica@gmail.com>
2023-01-29 21:15:21 +0100

    Adding SrcBuild kotlin DSL

---
+++ buildSrc/src/main/java/AndroidX.kt
@@ -0,0 +1,27 @@
+object AndroidX {
+    private const val lifecycleVersionKtx = "2.3.1"
+    const val lifecycleLivedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersionKtx"
+    const val lifecycleViewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersionKtx"
+
+    private const val lifecycleVersion = "2.2.0"
+    const val lifecycleExtension = "androidx.lifecycle:lifecycle-extensions:$lifecycleVersion"
+
+    private const val roomVersion = "2.3.0"
+    const val room = "androidx.room:room-runtime:$roomVersion"
+    const val roomKapt = "androidx.room:room-compiler:$roomVersion"
+
+    private const val navigationVersionKtx = "2.3.5"
+    const val navigationFragmentKtx = "androidx.navigation:navigation-fragment-ktx:$navigationVersionKtx"
+    const val navigationUIKtx = "androidx.navigation:navigation-ui-ktx:$navigationVersionKtx"
+
+    private const val vectorDrawableVersion = "1.1.0"
+    const val vectorDrawable = "androidx.vectordrawable:vectordrawable:$vectorDrawableVersion"
+
+    private const val constrainLayoutVersion = "2.4.0"
+    const val constrainLayout = "androidx.constraintlayout:constraintlayout:$constrainLayoutVersion"
+
+
+    // TODO: CHECK IF DEPENDENCY STILL NEEDED
+    private const val legacySupportVersion = "1.1.0"
+    private const val legacySupport = "androidx.legacy:legacy-support-v4:$legacySupportVersion"
+}
